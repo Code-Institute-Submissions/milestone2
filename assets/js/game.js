ed54fac4a6cfb9377e2ba63d58aa3e38aaa2ed47
Phil Surgenor <philsurgenor87@gmail.com>
2018-03-15 17:29:58 +0000

    Check User Sequence Added

--- assets/js/game.js
+++ assets/js/game.js
@@ -103,6 +103,12 @@ $(document).ready(function() {
         // The userSequence and gameSequence are compared to each other
         // if they match, return true
         // if they don't match return false
+        for(i=0; i<userSequence.length; i++) {
+            if(userSequence[i] != gameSequence[i]) {
+                return false;
+            } 
+        }
+        return true;
     }
     
     
