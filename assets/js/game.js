3263161d47af4be7309d13a9593d2ba7c6558bfb
Phil Surgenor <philsurgenor87@gmail.com>
2018-03-23 17:12:54 +0000

    reset button attr added to help with bug when reset button pressed setInterval changed to setTimeout for sequences

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,7 +11,18 @@ $(document).ready(function() {
     var highScore    = 0;
     var gameSequence = [];
     var userSequence = [];
-    var isCorrect    = false;
+    var resetClicked = false;
+    
+    
+    //Disables Reset Button
+    function resetDisabled() {
+        $("#reset").prop("disabled", true);
+        
+        setTimeout(function() {
+            $("#reset").prop("disabled", false);
+        }, 5000);
+    }
+    
     
     
     //Makes buttons light up
@@ -24,21 +35,32 @@ $(document).ready(function() {
     }
     
     
+    
+    //Game Over Sequence
+    
+    
     //Start Light Sequence
     function startLightSequence() {
         // each colour will light in a sequence one after the other
         var startSequence = [0,1,3,2,0,1,3,2,0,1,3,2,0];
-        var sequenceInterval = setInterval(function() {
+        
+        setTimeout(function startSequenceInterval() {
             var id = startSequence[index];
-            lightUp(id);
-            index++;
+                lightUp(id);
+                index++;
+                
+                console.log("Index - " + index);
             
             if(index == startSequence.length) {
-                clearInterval(sequenceInterval);
+                return clearTimeout(startSequenceInterval);
             }
-            
+            setTimeout(startSequenceInterval, 200);
+           
         }, 200);
+        
     }
+    
+    
     
     // Randon number from 1 to 3 added to game sequence
     // This number is pushed into an array gameSequence[]
@@ -46,6 +68,7 @@ $(document).ready(function() {
         var randomNum = Math.floor(Math.random()*4);
         gameSequence.push(randomNum);
     }
+    
     
     
     //Display Level
@@ -59,9 +82,9 @@ $(document).ready(function() {
 
     
     
-    
     function gameSequenceGen() {
         // level goes up by one
+        resetClicked = false;
         level++;
         displayLevel();
         //TEST
@@ -75,16 +98,25 @@ $(document).ready(function() {
         
         // Each number 0 to 3 represents a colour that colour will light and play its sound
         index=0;
-        
-        var sequenceInterval = setInterval(function() {
+         
+        setTimeout(function sequenceInterval() {
             var id = gameSequence[index];
-            lightUp(id);
-            index++;
+                lightUp(id);
+                index++;
+                
+                console.log("Index - " + index);
             
             if(index == gameSequence.length) {
-                clearInterval(sequenceInterval);
+                return clearTimeout(sequenceInterval);
+            } else if( resetClicked ) {
+               return clearTimeout(sequenceInterval);
             }
+                
+            setTimeout(sequenceInterval, 650);
+           
         }, 650);
+        
+        
     }
     
     // When colour button clicked
@@ -129,14 +161,18 @@ $(document).ready(function() {
     
     //When play or reset button clicked
     $("#play, #reset").on("click", function() {
+        resetClicked = true; 
         startLightSequence();
+        console.log("Start Sequence Finished");
         index        = 0;
         level        = 0;
         score        = 0;
         gameSequence = [];
         userSequence = [];
         displayLevel();
+        resetDisabled();
         setTimeout(gameSequenceGen, 5000);
     });
     
+
 });
